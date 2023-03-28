6821f04d136f7f2e15ae0201c2b8be4ba5f35d9a
Ericawanja <wanjaerica@gmail.com>
2023-03-28 23:10:47 +0300

    data binding

--- Introduction/angularapp/src/app/app.module.ts
+++ Introduction/angularapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FooterComponent } from 'src/Footer/footer.component';
 import { HeaderComponent } from 'src/Header/header.component';
 
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { BodyComponent } from './body/body.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
