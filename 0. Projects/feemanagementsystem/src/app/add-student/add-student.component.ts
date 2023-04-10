c3d1f5b0f13dbd0a4030977056abe7852c3d9ca5
Ericawanja <wanjaerica@gmail.com>
2023-04-10 23:07:55 +0300

    add student form

--- 0. Projects/feemanagementsystem/src/app/add-student/add-student.component.ts
+++ 0. Projects/feemanagementsystem/src/app/add-student/add-student.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-student.component.css']
 })
 export class AddStudentComponent {
+  genders:string[] =[
+    'male', 'female'
+  ]
 
 }
