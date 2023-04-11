97a9a7945665c90178d5b2798f168d642267bcba
Ericawanja <wanjaerica@gmail.com>
2023-04-11 16:34:59 +0300

    observables

--- 0. Projects/feemanagementsystem/src/app/add-student/add-student.component.ts
+++ 0. Projects/feemanagementsystem/src/app/add-student/add-student.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { StudentsService } from 'src/services/studentsFees';
 
 @Component({
@@ -9,11 +10,12 @@ import { StudentsService } from 'src/services/studentsFees';
 })
 export class AddStudentComponent {
   @ViewChild('studentRegistrationform') formData!: NgForm;
-  constructor(private studentService: StudentsService) {}
+  constructor(private studentService: StudentsService,private router:Router, private route:ActivatedRoute) {}
   genders: string[] = ['male', 'female'];
   handleSubmit(): void {
     console.group(this.formData.value);
     let id: string = Math.floor(Math.random() * 2).toString();
     this.studentService.addStudents({ ...this.formData.value, id, paid: 0 });
+    this.router.navigate([''], {relativeTo:this.route})
   }
 }
--- Routing/routingdemoapp/src/app/app.component.ts
+++ Routing/routingdemoapp/src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'routingdemoapp';
-  constructor(private router:Router){}
-  loadProductsPage():void{
-this.router.navigate(['products'])
+  firstSub!: Observer<any>;
+  constructor(private router: Router) {}
+  loadProductsPage(): void {
+    this.router.navigate(['products']);
+  }
+
+  obs = new Observable((observer) => {
+    let count = 0;
+    setInterval(() => {
+      observer.next(count);
+      count++;
+    }, 1000);
+  });
+  ngOnInit(): void {
+    //custom observable
+    
+
+    this.obs.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => console.log('completed')
+    );
+  }
+  ngOnDestroy(): void {
+    //unsubscribe the observable
   }
 }
